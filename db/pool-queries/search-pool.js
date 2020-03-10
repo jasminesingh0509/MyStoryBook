b9c102ec8e6bb7a5fbe6dd4585e9c26d92490f7f
Jasmine <jasmine_singh1992@hotmail.com>
2020-03-10 17:17:05 +0000

    trying to merge

--- db/pool-queries/search-pool.js
+++ db/pool-queries/search-pool.js
@@ -1,50 +1,48 @@
 const pool = require(`./pool-query`);
 
-const browse = (cb) => {
+const browse = cb => {
   pool
     .query(`SELECT * FROM stories`)
-    .then((res) => {
+    .then(res => {
       cb(null, res.rows);
     })
-    .catch((err) => cb(err));
+    .catch(err => cb(err));
 };
 
 const read = (id, cb) => {
   pool
-    .query(`SELECT * FROM stories WHERE id = $1`, [ id ])
-    .then((res) => {
+    .query(`SELECT * FROM stories WHERE id = $1`, [id])
+    .then(res => {
       cb(null, res.rows[0]);
     })
-    .catch((err) => cb(err));
+    .catch(err => cb(err));
 };
 
 const usersWithName = (name, cb) => {
   pool
-    .query(`SELECT * FROM users WHERE name = $1`, [ name ])
-    .then((res) => {
+    .query(`SELECT * FROM users WHERE name = $1`, [name])
+    .then(res => {
       cb(null, res.rows[0]);
     })
-    .catch((err) => cb(err));
+    .catch(err => cb(err));
 };
-<<<<<<< HEAD
-
-const getStory = function(id) {
-=======
 const getStory = (id, cb) => {
->>>>>>> joseph/routes
   // .text retrives the text from story object
-  pool.query(`SELECT * FROM stories WHERE id = $1`, [ id ]).then((res) => cb(res.rows[0])).catch((err) => cb(err));
+  pool
+    .query(`SELECT * FROM stories WHERE id = $1`, [id])
+    .then(res => cb(res.rows[0]))
+    .catch(err => cb(err));
 };
 
 const edit = (id, stories, cb) => {
-  const sql = 'UPDATE stories SET text = $2 WHERE id = $1;';
-  const args = [ id, stories ];
+  const sql = "UPDATE stories SET text = $2 WHERE id = $1;";
+  const args = [id, stories];
   pool
     .query(sql, args)
     .then(() => {
-      cb(null, 'story updated succesfully');
+      cb(null, "story updated succesfully");
     })
-    .catch((err) => cb(err));
+    .catch(err => cb(err));
 };
 
 const getStoryByUser = function(id, cb) {
@@ -54,16 +52,23 @@ const getStoryByUser = function(id, cb) {
 From stories
 Join users ON users.id = user_id
 WHERE users.id = $1`,
-      [ id ],
+      [id]
     )
-    .then((res) => cb(res.rows[0]));
+    .then(res => cb(res.rows[0]));
 };
 getStoryByUser(1);
 const addStory = function(story) {
   return pool.query(
     `INSERT INTO stories
 (user_id, text, created_at, updated_at, title, picture_url, is_completed) values ($1, $2, $3, $4, $5, $6) RETURNING *`,
-    [ stories.user_id, stories.text, stories.created_at, stories.updated_at, stories.title, stories.picture_url ],
+    [
+      stories.user_id,
+      stories.text,
+      stories.created_at,
+      stories.updated_at,
+      stories.title,
+      stories.picture_url
+    ]
   );
 };
 
@@ -76,10 +81,10 @@ From contributions
 Join stories on stories.id = story_id
 WHERE stories.id = $1
 ORDER BY contributions.order_by`,
-      [ story ],
+      [story]
     )
-    .then((res) => res.rows)
-    .catch((err) => console.log(err));
+    .then(res => res.rows)
+    .catch(err => console.log(err));
 };
 
 const addContributionsToStory = function(story) {
@@ -95,20 +100,20 @@ const getCompletedStory = function() {
 FROM contributions
 JOIN stories on stories.id = story_id
 WHERE stories.is_completed = true
-ORDER BY contributions.order_by`,
+ORDER BY contributions.order_by`
     )
-    .then((res) => console.log(res.rows));
+    .then(res => console.log(res.rows));
 };
 
 const del = (id, cb) => {
-  const sql = 'DELETE FROM stories WHERE id = $1;';
-  const args = [ id ];
+  const sql = "DELETE FROM stories WHERE id = $1;";
+  const args = [id];
   pool
     .query(sql, args)
     .then(() => {
-      cb(null, 'deleted sucessfully');
+      cb(null, "deleted sucessfully");
     })
-    .catch((err) => cb(err));
+    .catch(err => cb(err));
 };
 
 //console.log(getCompletedStory());
@@ -133,9 +138,5 @@ module.exports = {
   addStory,
   usersWithName,
   browse,
-<<<<<<< HEAD
-  read,
-=======
-  edit,
->>>>>>> joseph/routes
+  edit
 };
