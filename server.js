1adcfc4735320bd8aa9db083274f52649d9e9bde
Jasmine <59420077+jasminesingh0509@users.noreply.github.com>
2020-04-29 12:39:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -48,16 +48,6 @@ app.use(
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
-
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
